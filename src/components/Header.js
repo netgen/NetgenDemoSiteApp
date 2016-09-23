eac8ccd3ce77fac9010e4e28aec8ef03a4056508
BornaP <borna@netgen.hr>
2016-09-23 14:48:10 +0200

    Added icons, logo and application header

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import React, { Component, PropTypes } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import Icon  from 'react-native-vector-icons/Entypo';
+
+
+
+const styles = StyleSheet.create({
+  header: {
+    height: 90,
+    backgroundColor: '#ef4134',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  leftItem: {
+    marginLeft: 15,
+  },
+  centerItem: {
+    height: 50,
+    width: 50,
+  },
+  rightItem: {
+    marginRight: 15,
+    transform: [{ rotate: '90deg' }]
+  },
+});
+
+export default class Header extends Component {
+  static propTypes = {
+    onMenuClick: PropTypes.func,
+    onSearchClick: PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const leftIcon = this.state.menuOpened ? 'cross' : 'menu';
+
+    return (
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => this.setState({ menuOpened: !this.state.menuOpened })}>
+          <Icon
+            name={leftIcon}
+            size={40}
+            color='#fff'
+            style={styles.leftItem} />
+        </TouchableOpacity>
+        <Image style={styles.centerItem}
+          source={require('../../assets/logo_simple.png')} />
+        <Icon
+          name='magnifying-glass'
+          size={32}
+          color='#fff'
+          style={styles.rightItem} />
+      </View>
+    );
+  }
+}
